e86014d182f0a0db2f57769a8f91ddd7f8106a4d
Ivy Lam <ivylam82@hotmail.com>
2020-05-30 17:01:44 +0800

    second version

--- products.py
+++ products.py
@@ -12,3 +12,9 @@ print (products[0][0])
 for p in products:
 	print(p)
 	print(p[0], '的價格是', p[1])
+
+# 'abc' + '123' = 'abc123'
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') 
