435cd3fa16f96c3c76da9fdf870b76c8c4eab3ee
Ivy Lam <ivylam82@hotmail.com>
2020-05-30 17:59:21 +0800

    third version

--- products.py
+++ products.py
@@ -6,7 +6,6 @@ while True:
 	price = input('請輸入商品價格')
 	products.append([name, price])
 print(products)
-
 print (products[0][0])
 
 for p in products:
@@ -15,6 +14,12 @@ for p in products:
 
 # 'abc' + '123' = 'abc123'
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品, 價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n') 
+
+data = [1, 3, 5, 7]
+with open('test.txt', 'w') as f:
+	for d in data:
+		f.write(str(d) + ',' + '\n')
